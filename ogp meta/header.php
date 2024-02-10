7228ee45d3135bbf7dbef73a0ee6f4f7121c239a
Shimizu yuki <shimizuyuki.freelance@gmail.com>
2024-02-10 18:19:28 +0900

    headタグにprefixを追加

--- ogp meta/header.php
+++ ogp meta/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
-
-<head>
+<!-- topページならばwebsite それ以外ならarticleを返す-->
+<?php $attribute = (is_home() || is_front_page()) ? 'website' : 'article'; ?> 
+<!-- ogpを使用することのの宣言を条件分岐で記述 -->
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $attribute; ?>: http://ogp.me/ns/<?php echo $attribute; ?>#">
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="description" content="プラグイン無しでogpのmetaタグを挿入するものです" />
